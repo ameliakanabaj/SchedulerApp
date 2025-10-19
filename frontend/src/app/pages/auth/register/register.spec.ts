d738c9f831b2f0093f4e2174ad68455e90599e83
Piotr Kosmowski <pkosmowski553@gmail.com>
2025-10-19 22:05:39 +0200

    adjusted unit tests

--- frontend/src/app/pages/auth/register/register.spec.ts
+++ frontend/src/app/pages/auth/register/register.spec.ts
@@ -1,26 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Register } from './register';
 import { ActivatedRoute } from '@angular/router';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
 
 describe('Register', () => {
-    let component: Register;
-    let fixture: ComponentFixture<Register>;
+    let sp: Spectator<Register>;
+    const createComponent = createComponentFactory({
+        component: Register,
+        providers: [
+            {
+                provide: ActivatedRoute,
+                useValue: { snapshot: { queryParams: {} } }
+            }
+        ]
+    })
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [Register],
-            providers: [
-                { provide: ActivatedRoute, useValue: { snapshot: { paramMap: new Map() } } },
-            ],
-        }).compileComponents();
-
-        fixture = TestBed.createComponent(Register);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+    beforeEach(() => {
+        sp = createComponent();
     });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    
+    describe('onRegister', () => {
+        it('should properly send register data', () => {  // TO-DO
+            sp.component.email = '';
+            sp.component.password = '';
+        });
     });
 });
