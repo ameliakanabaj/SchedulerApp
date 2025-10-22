33564c34a809282a3d9f1034b2a7a854a02c649f
Piotr Kosmowski <pkosmowski553@gmail.com>
2025-10-22 14:22:46 +0200

    feat(ui): implement organisations page

---
+++ frontend/src/app/pages/organisations/organisations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organisations',
+  imports: [],
+  templateUrl: './organisations.component.html',
+  styleUrl: './organisations.component.css'
+})
+export class OrganisationsComponent {
+
+}
