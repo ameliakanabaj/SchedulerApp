bcf0de8e0d8c7823a2f6f22682b264c9988acb50
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2025-10-28 11:04:03 +0100

    fixed problems with jest configurations and migrated from spectator to TestingBed and BrowserTestingModule

--- frontend/setup-jest.ts
+++ frontend/setup-jest.ts
@@ -1,10 +1,12 @@
 import 'zone.js';
 import 'zone.js/testing';
-import '@testing-library/jest-dom';
-import { TestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserTestingModule,
+  platformBrowserTesting
+} from '@angular/platform-browser/testing';
 
-TestBed.initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+getTestBed().initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting()
 );
