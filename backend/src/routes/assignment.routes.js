d259999041892894112820cf787fe38577e3beac
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2025-11-20 14:16:08 +0100

    added swagger documentation for auth routes

--- backend/src/routes/assignment.routes.js
+++ backend/src/routes/assignment.routes.js
@@ -43,30 +43,6 @@ const { createAssignmentValidation, updateAssignmentValidation } = require("../v
  *         description: Shift or user not found
  */
 router.post("/", auth(["ORG_ADMIN", "GLOBAL_ADMIN"]), createAssignmentValidation, controller.createAssignment);
-/**
- * @swagger
- * /api/assignments/assignments/{id}:
- *   get:
- *     summary: gets assignment by assignment id
- *     tags: [Assignments]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *         required: true
- *         description: ID of assignment
- *     responses:
- *       200:
- *         description: Assignment found
- *       403:
- *         description: Forbidden
- *       404:
- *         description: Assignment not found
- */
-router.get("/assignments/:id", controller.getAssignmentById);
 
 /**
  * @swagger
@@ -120,15 +96,15 @@ router.get("/user/:user_id", auth(["ORG_ADMIN", "GLOBAL_ADMIN", "EMPLOYEE"]), co
 
 /**
  * @swagger
- * /api/assignments/user/{user_id}:
+ * /api/assignments/{id}:
  *   get:
- *     summary: gets assignment by user id
+ *     summary: gets assignment by assignment id
  *     tags: [Assignments]
  *     security:
  *       - bearerAuth: []
  *     parameters:
  *       - in: path
- *         name: shift_id
+ *         name: id
  *         schema:
  *           type: integer
  *         required: true
@@ -139,10 +115,65 @@ router.get("/user/:user_id", auth(["ORG_ADMIN", "GLOBAL_ADMIN", "EMPLOYEE"]), co
  *       403:
  *         description: Forbidden
  *       404:
- *         description: User not found
+ *         description: Assignment not found
  */
 router.get("/:id", auth(["ORG_ADMIN", "GLOBAL_ADMIN", "EMPLOYEE"]), controller.getAssignmentById);
+
+/**
+ * @swagger
+ * /api/assignments/{id}:
+ *   patch:
+ *     summary: Updates assignment
+ *     tags: [Assignments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               role_on_shift:
+ *                 type: string
+ *                 example: "MANAGER"
+ *     responses:
+ *       200:
+ *         description: Updated successfully
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Assignment not found
+ */
 router.patch("/:id", auth(["ORG_ADMIN", "GLOBAL_ADMIN"]), updateAssignmentValidation, controller.updateAssignment);
+
+/**
+ * @swagger
+ * /api/assignments/{id}:
+ *   delete:
+ *     summary: Deletes assignment
+ *     tags: [Assignments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Assignment deleted
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: Assignment not found
+ */
 router.delete("/:id", auth(["ORG_ADMIN", "GLOBAL_ADMIN"]), controller.deleteAssignment);
 
 module.exports = router;
