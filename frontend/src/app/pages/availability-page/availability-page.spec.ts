7f0d743e85c72246807054be4f093182c2a50e13
Piotr Kosmowski <pkosmowski553@gmail.com>
2025-11-21 17:42:13 +0100

    unit tests

--- frontend/src/app/pages/availability-page/availability-page.spec.ts
+++ frontend/src/app/pages/availability-page/availability-page.spec.ts
@@ -1,23 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { AvailabilityPage } from './availability-page';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { Toastr } from '../../shared/services/toastr/toastr';
 
 describe('AvailabilityPage', () => {
-  let component: AvailabilityPage;
-  let fixture: ComponentFixture<AvailabilityPage>;
+    let sp: Spectator<AvailabilityPage>;
+    const createComponent = createComponentFactory({
+        component: AvailabilityPage,
+        providers: [
+            {
+                provide: HttpClient,
+                useValue: {},
+            },
+            {
+                provide: Toastr,
+                useValue: {},
+            }
+        ]
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AvailabilityPage]
-    })
-    .compileComponents();
+    beforeEach(() => {
+        sp = createComponent();
+    });
 
-    fixture = TestBed.createComponent(AvailabilityPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    describe('init', () => {
+        it('should create', () => {
+            expect(sp).not.toBeNull();
+        });
+    });
 });
