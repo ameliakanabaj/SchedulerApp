97090163a4bdf70a1385dd167bfb4d45d18b5038
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2025-12-07 18:38:42 +0100

    changed migration

--- backend/prisma/migrations/fix_schedule_relations/migration.sql
+++ backend/prisma/migrations/fix_schedule_relations/migration.sql
@@ -10,16 +10,24 @@ CREATE TABLE "Schedule" (
     "generated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "status" "ScheduleStatus" NOT NULL DEFAULT 'PENDING',
     "deadline_generate_date" TIMESTAMP(3) NOT NULL,
-
     CONSTRAINT "Schedule_pkey" PRIMARY KEY ("schedule_id")
 );
+
+-- Add column to Assignment
+ALTER TABLE "Assignment"
+ADD COLUMN "schedule_id" INTEGER;
 
 -- CreateIndex
 CREATE INDEX "Schedule_organization_id_idx" ON "Schedule"("organization_id");
 
 -- AddForeignKey
-ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_schedule_id_fkey" FOREIGN KEY ("schedule_id") REFERENCES "Schedule"("schedule_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Schedule"
+ADD CONSTRAINT "Schedule_organization_id_fkey"
+FOREIGN KEY ("organization_id") REFERENCES "Organization"("organization_id")
+ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Schedule" ADD CONSTRAINT "Schedule_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "Organization"("organization_id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
+ALTER TABLE "Assignment"
+ADD CONSTRAINT "Assignment_schedule_id_fkey"
+FOREIGN KEY ("schedule_id") REFERENCES "Schedule"("schedule_id")
+ON DELETE RESTRICT ON UPDATE CASCADE;
