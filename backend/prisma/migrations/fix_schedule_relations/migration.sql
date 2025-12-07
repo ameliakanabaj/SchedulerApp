2be23f3059f127d004fb16b0077450724dc000b7
Amelia Kanabaj <a.kanabaj.124@studms.ug.edu.pl>
2025-12-07 17:36:50 +0100

    adding prisma migrations

---
+++ backend/prisma/migrations/fix_schedule_relations/migration.sql
@@ -0,0 +1,117 @@
+-- CreateEnum
+CREATE TYPE "ScheduleStatus" AS ENUM ('PENDING', 'GENERATED', 'FAILED', 'APPROVED', 'NOT_APPROVED');
+
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('GLOBAL_ADMIN', 'ORG_ADMIN', 'EMPLOYEE');
+
+-- CreateTable
+CREATE TABLE "Organization" (
+    "organization_id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("organization_id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "user_id" SERIAL NOT NULL,
+    "organization_id" INTEGER,
+    "first_name" VARCHAR(255) NOT NULL,
+    "last_name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'EMPLOYEE',
+    "position" VARCHAR(255),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "Shift" (
+    "shift_id" SERIAL NOT NULL,
+    "organization_id" INTEGER NOT NULL,
+    "start_time" TIMESTAMP(3) NOT NULL,
+    "end_time" TIMESTAMP(3) NOT NULL,
+    "required_people" INTEGER,
+    "place" VARCHAR(255),
+
+    CONSTRAINT "Shift_pkey" PRIMARY KEY ("shift_id")
+);
+
+-- CreateTable
+CREATE TABLE "Availability" (
+    "availability_id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "start_time" TIMESTAMP(3) NOT NULL,
+    "end_time" TIMESTAMP(3) NOT NULL,
+    "comments" VARCHAR(500),
+
+    CONSTRAINT "Availability_pkey" PRIMARY KEY ("availability_id")
+);
+
+-- CreateTable
+CREATE TABLE "Assignment" (
+    "assignment_id" SERIAL NOT NULL,
+    "shift_id" INTEGER NOT NULL,
+    "schedule_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "role_on_shift" VARCHAR(255),
+
+    CONSTRAINT "Assignment_pkey" PRIMARY KEY ("assignment_id")
+);
+
+-- CreateTable
+CREATE TABLE "Schedule" (
+    "schedule_id" SERIAL NOT NULL,
+    "organization_id" INTEGER NOT NULL,
+    "date_from" TIMESTAMP(3) NOT NULL,
+    "date_to" TIMESTAMP(3) NOT NULL,
+    "generated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "ScheduleStatus" NOT NULL DEFAULT 'PENDING',
+    "deadline_generate_date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Schedule_pkey" PRIMARY KEY ("schedule_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "idx_user_email" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "idx_availability_user_id" ON "Availability"("user_id");
+
+-- CreateIndex
+CREATE INDEX "idx_assignment_shift_id" ON "Assignment"("shift_id");
+
+-- CreateIndex
+CREATE INDEX "idx_assignment_user_id" ON "Assignment"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "uq_shift_user" ON "Assignment"("shift_id", "user_id");
+
+-- CreateIndex
+CREATE INDEX "Schedule_organization_id_idx" ON "Schedule"("organization_id");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "Organization"("organization_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Shift" ADD CONSTRAINT "Shift_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "Organization"("organization_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Availability" ADD CONSTRAINT "Availability_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("user_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_shift_id_fkey" FOREIGN KEY ("shift_id") REFERENCES "Shift"("shift_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("user_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Assignment" ADD CONSTRAINT "Assignment_schedule_id_fkey" FOREIGN KEY ("schedule_id") REFERENCES "Schedule"("schedule_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Schedule" ADD CONSTRAINT "Schedule_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "Organization"("organization_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
