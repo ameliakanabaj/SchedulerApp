bbebc9d6ee5b2b3960b7228b41dfd23753260847
Paulina <paulina.krajnik04@gmail.com>
2026-01-19 00:10:39 +0100

    fix: notification controller path

--- backend/src/routes/notifications.routes.js
+++ backend/src/routes/notifications.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const notificationController = require("../controllers/notification.controller");
+const notificationController = require("../controllers/notifications.controller");
 const auth = require("../middlewares/auth.middleware");
 
 router.get("/", auth(["EMPLOYEE", "ORG_ADMIN", "GLOBAL_ADMIN"]), notificationController.getMyNotifications);
