2394d73d2a1d59151db2b51fc0bb3cd40432ed02
Piotr Kosmowski <pkosmowski553@gmail.com>
2026-01-18 10:35:18 +0100

    added loadings

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { Modal, User } from '@app/shared/services';
 import { PasswordReset } from '@app/features/password-reset/password-reset';
@@ -7,10 +7,11 @@ import { Authentication } from '@app/core';
 import { Schedule } from '@app/shared/services/schedule/schedule';
 import { ViewOnlyCalendar } from '@app/features/view-only-calendar/view-only-calendar';
 import { UserModel } from '@app/models';
+import { Loading } from '@app/shared/components/loading/loading';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ViewOnlyCalendar, DatePipe, RouterLink],
+  imports: [ViewOnlyCalendar, DatePipe, RouterLink, Loading],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
@@ -18,6 +19,7 @@ export class Dashboard implements OnInit {
     userHasOrganization = true;
     today = new Date();
     schedule?: number;
+    isLoading = signal(true);
 
     mode: 'view' | 'availability' | 'admin'  = 'view';
 
@@ -37,7 +39,7 @@ export class Dashboard implements OnInit {
         const userId = this.authService.getUserId();
 
         this.userService.getById(Number(userId)).subscribe(user => {
-            console.log(user);
+            this.isLoading.set(false);
         });
 
         if (!this.authService.getOrgId()) {
@@ -52,13 +54,14 @@ export class Dashboard implements OnInit {
     }
 
     loadSchedule(): void {
+        this.isLoading.set(true);
         const orgId = this.authService.getOrgId();
         if (orgId) {
             this.scheduleService.getAllByOrganization(orgId).subscribe(schedules => {
                 if (schedules.length > 0) {
                     const valid = schedules.find(s => s.status !== 'FAILED' && s.status !== 'PENDING');
                     this.schedule = valid?.schedule_id;
-                    
+                    this.isLoading.set(false);
                 }
             });
         }
--- frontend/src/app/pages/organizations-page/organizations-page.ts
+++ frontend/src/app/pages/organizations-page/organizations-page.ts
@@ -1,17 +1,19 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Modal, Toastr } from '@app/shared/services';
+import { Modal } from '@app/shared/services';
 import { AddNewMemberModal, OrganizationCreationModal, UserCard } from '@app/shared/components';
 import { Organization } from '@app/shared/services/organization/organization';
 import { OrganizationModel } from '@app/models';
+import { Loading } from '@app/shared/components/loading/loading';
 
 @Component({
   selector: 'app-organizations-page',
-  imports: [UserCard],
+  imports: [UserCard, Loading],
   templateUrl: './organizations-page.html',
   styleUrl: './organizations-page.scss'
 })
 export class OrganizationsPage implements OnInit {
     organization = signal<OrganizationModel | null>(null);
+    isLoading = signal(true);
 
     private readonly modalService = inject(Modal);
     private readonly organizationService = inject(Organization)
@@ -26,11 +28,12 @@ export class OrganizationsPage implements OnInit {
         modalRef.afterClosed$.subscribe((res: any) => {
             if (res) {
                 this.organization.set(res.organization);
+                this.organization()?.users.push(...res.organization.users);
             }
         });
     }
 
-openAddNewMemberModal(): void {
+    openAddNewMemberModal(): void {
         const modalRef = this.modalService.openModal(AddNewMemberModal, { data: {
             organization: this.organization(),
         }});
@@ -43,10 +46,12 @@ openAddNewMemberModal(): void {
     }
 
     private getOrganization(): void {
+        this.isLoading.set(true);
         this.organizationService.getAll().subscribe((res) => {
             if (res.length > 0) {
                 this.organization.set(res[0]);
             }
+            this.isLoading.set(false);
         });
     }
 }
